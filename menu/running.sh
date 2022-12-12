0da05323fe8a18a9e1fddfec23284eec06172194
oktest145 <you@example.com>
2022-12-12 14:10:15 +0530

    Previous

--- menu/running.sh
+++ menu/running.sh
@@ -247,8 +247,8 @@ kernelku=$(uname -r)
 
 # DNS PATCH
 #tipeos2=$(uname -m)
-Name=$(curl -sS https://raw.githubusercontent.com/oktest145/firessh/main/permission/ip | grep $MYIP | awk '{print $2}')
-Exp=$(curl -sS https://raw.githubusercontent.com/oktest145/firessh/main/permission/ip | grep $MYIP | awk '{print $3}')
+Name=$(curl -sS https://raw.githubusercontent.com/oktest145/firessh/main/permission/ip  | grep $MYIP | awk '{print $2}')
+Exp=$(curl -sS https://raw.githubusercontent.com/oktest145/firessh/main/permission/ip  | grep $MYIP | awk '{print $3}')
 # GETTING DOMAIN NAME
 Domen="$(cat /etc/xray/domain)"
 echo -e ""
