b3a097deb3d5c452da00b9736950d7758e3a4a68
oktest145 <83488344+oktest145@users.noreply.github.com>
2023-04-10 07:55:26 +0530

    update slowdns.sh

--- slowdns.sh
+++ slowdns.sh
@@ -38,3 +38,11 @@ apt install -y sudo gnutls-bin
 apt install -y dos2unix debconf-utils
 service cron reload
 service cron restart
+
+cd
+echo "Port 2222" >> /etc/ssh/sshd_config
+echo "Port 2269" >> /etc/ssh/sshd_config
+sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+service ssh restart
+service sshd restart
+
