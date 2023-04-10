c2ebbc6ab019b4c541016f1a929aa14bb241036e
oktest145 <83488344+oktest145@users.noreply.github.com>
2023-04-10 07:53:55 +0530

    update slowdns.sh

--- slowdns.sh
+++ slowdns.sh
@@ -1,1 +1,40 @@
+#!/bin/bash
 
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+netfilter-persistent save
+netfilter-persistent reload
+
+cd
+#delete directory
+rm -rf /root/nsdomain
+rm nsdomain
+
+#input nameserver manual to cloudflare
+read -rp "Masukkan domain: " -e domain
+
+read -rp "Masukkan Subdomain: " -e sub
+SUB_DOMAIN=${sub}.${domain}
+NS_DOMAIN=slowdns-${SUB_DOMAIN}
+echo $NS_DOMAIN > /root/nsdomain
+
+nameserver=$(cat /root/nsdomain)
+apt update -y
+apt install -y python3 python3-dnslib net-tools
+apt install ncurses-utils -y
+apt install dnsutils -y
+#apt install golang -y
+apt install git -y
+apt install curl -y
+apt install wget -y
+apt install ncurses-utils -y
+apt install screen -y
+apt install cron -y
+apt install iptables -y
+apt install -y git screen whois dropbear wget
+#apt install -y pwgen python php jq curl
+apt install -y sudo gnutls-bin
+#apt install -y mlocate dh-make libaudit-dev build-essential
+apt install -y dos2unix debconf-utils
+service cron reload
+service cron restart
